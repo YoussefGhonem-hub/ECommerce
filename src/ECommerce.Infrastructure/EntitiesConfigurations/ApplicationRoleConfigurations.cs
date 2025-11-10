f634ad56aebf8c199a6b52f125efeff3e7d753e6
Youssef Ghonem <youssef.fcih@gmail.com>
2025-11-10 20:56:38 +0200

    fixes

---
+++ src/ECommerce.Infrastructure/EntitiesConfigurations/ApplicationRoleConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerce.Infrastructure.EntitiesConfigurations;
+
+public class ApplicationRoleConfigurations : IEntityTypeConfiguration<ApplicationRole>
+{
+    public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+    {
+
+    }
+}
+
